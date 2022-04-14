901f2022c606c9feb693e91e7f4c439f9f8cd1a1
nattec <73382455+nattec@users.noreply.github.com>
2022-04-14 12:05:14 +0200

    Update train.py

--- train.py
+++ train.py
@@ -2,11 +2,6 @@
 """
 Train a YOLOv5 model on a custom dataset.
 
-Models and datasets download automatically from the latest YOLOv5 release.
-Models: https://github.com/ultralytics/yolov5/tree/master/models
-Datasets: https://github.com/ultralytics/yolov5/tree/master/data
-Tutorial: https://github.com/ultralytics/yolov5/wiki/Train-Custom-Data
-
 Usage:
     $ python path/to/train.py --data coco128.yaml --weights yolov5s.pt --img 640  # from pretrained (RECOMMENDED)
     $ python path/to/train.py --data coco128.yaml --weights '' --cfg yolov5s.yaml --img 640  # from scratch
